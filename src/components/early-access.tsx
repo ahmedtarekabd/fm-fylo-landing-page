1f45d2e8989850ff66ac1ee411c865ceec7e4a62
Ahmed Tarek <ahmedtarekabd2002@gmail.com>
2024-08-01 15:28:44 +0300

    Initial Commit.

---
+++ src/components/early-access.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EarlyAccess = () => {
+  return (
+    <div>EarlyAccess</div>
+  )
+}
+
+export default EarlyAccess
---
+++ src/components/navbar.tsx
@@ -0,0 +1,77 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import logo from "@/public/assets/images/logo.svg";
+import MenuOpen from "@/public/assets/images/icon-menu.svg";
+import MenuClose from "@/public/assets/images/icon-menu-close.svg";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <NavigationMenu className="py-10 justify-between items-center">
+      <NavigationMenuList>
+        <NavigationMenuItem className="flex items-center gap-2">
+          <Image priority src={logo} className="w-12" alt="Logo" />
+          Fylo
+        </NavigationMenuItem>
+      </NavigationMenuList>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
+
+const NavbarList = () => {
+  return (
+    <NavigationMenuList className="flex flex-col items-start md:flex-row">
+      <NavigationMenuItem className="p-2 py-4 text-xl font-medium md:font-light">
+        <Link href="/" legacyBehavior passHref>
+          <NavigationMenuLink>Home</NavigationMenuLink>
+        </Link>
+      </NavigationMenuItem>
+      <NavigationMenuItem className="p-2 py-4 text-xl font-medium md:font-light">
+        <Link href="/new" legacyBehavior passHref>
+          <NavigationMenuLink>New</NavigationMenuLink>
+        </Link>
+      </NavigationMenuItem>
+      <NavigationMenuItem className="p-2 py-4 text-xl font-medium md:font-light">
+        <Link href="/popular" legacyBehavior passHref>
+          <NavigationMenuLink>Popular</NavigationMenuLink>
+        </Link>
+      </NavigationMenuItem>
+      <NavigationMenuItem className="p-2 py-4 text-xl font-medium md:font-light">
+        <Link href="/trending" legacyBehavior passHref>
+          <NavigationMenuLink>Trending</NavigationMenuLink>
+        </Link>
+      </NavigationMenuItem>
+      <NavigationMenuItem className="p-2 py-4 text-xl font-medium md:font-light">
+        <Link href="/categories" legacyBehavior passHref>
+          <NavigationMenuLink>Categories</NavigationMenuLink>
+        </Link>
+      </NavigationMenuItem>
+    </NavigationMenuList>
+  );
+};
+
+export default Navbar;
